6dfe027a71e3f14058c82f4f7a7aa2e292d576c1
phoenix <ascronz@gmail.com>
2013-08-25 19:47:02 +0300

    xhttp demo

--- content_core.js
+++ content_core.js
@@ -4,3 +4,61 @@ s.onload = function() {
     this.parentNode.removeChild(this);
 };
 (document.head||document.documentElement).appendChild(s);
+
+var EVENT_NAME = "ER_connectExtension";
+
+var REQUEST_TYPE_XHTTP = 'xhttp_request';
+
+var requestResolver = (function(){
+    function init() {
+        document.addEventListener(EVENT_NAME, function(e) {
+            var from = e.target;
+            if (from) {
+                resolve(JSON.parse(from.value), function(response){
+                    var event = document.createEvent('Events');
+                    event.initEvent('action', true, false);
+                    from.value = JSON.stringify(response);
+                    from.dispatchEvent(event);
+                });
+            }
+        }, true);
+    }
+
+    function resolve (request, callback) {
+        switch (request.type) {
+            case REQUEST_TYPE_XHTTP:
+                xhttp(request.body, callback);
+        }
+    }
+
+    function xhttp (params, callback) {
+        var request = new XMLHttpRequest();
+
+        request.onreadystatechange = function() {
+            if (params.onreadystatechange) {
+                params.onreadystatechange(request);
+            }
+
+            if (request.readyState == 4) {
+                if (request.status >= 200 && request.status < 400) {
+                    if (callback) {
+                        callback(request)
+                    }
+                } else {
+                    if (callback) {
+                        callback({'status': 0});
+                    }
+                }
+            }
+        }
+
+        request.open(params.method, params.url, true)
+        if (params.headers) for (nname in params.headers)
+            request.setRequestHeader(nname, params.headers[nname])
+
+        request.send(params.data)
+        return request
+    }
+
+    return {init: init};
+})().init();
--- core.js
+++ core.js
@@ -1,10 +1,42 @@
 (function(){
 
-    function init() {
-        plugins_chatcoords();
+    var EVENT_NAME = "ER_connectExtension";
+
+    var REQUEST_TYPE_XHTTP = 'xhttp_request';
+
+    function init () {
+        pluginContainer.load();
+
+        xhttp({
+            method: "POST",
+            url: "http://sp.erclans.ru/evgeska_prof.php?calc=heroesinfo",
+            data: "prof=phoenix&submit=1"
+        }, function(response){
+            console.log(response);
+        });
     }
 
-    function plugins_chatcoords() {
+    var pluginContainer = (function(plugins){
+        var container = plugins;
+
+        function load () {
+            for (plugin in container) {
+                if (container.hasOwnProperty(plugin) && plugin.enabled){
+                    plugin.func();
+                }
+            }
+        }
+
+        function call (plugin) {
+            return container[plugin];
+        }
+
+        return {load: load, call: call};
+    })({
+        chatCoords: {func: plugins_chatcoords, enabled: true}
+    });
+
+    function plugins_chatcoords () {
         chat.formatSmilies = overloader(chat, chat.formatSmilies, function(){
             arguments[0] = arguments[0].replace(
                 /(\d{1,3})[: \.\-](\d{1,3})/ig,
@@ -25,6 +57,25 @@
             }
             return result;
         };
+    }
+
+    function sendMessage (data, callback) {
+        var d = document.createElement("textarea"),
+            e = document.createEvent("Events");
+        d.style.cssText = "display:none;";
+        d.value = data == null ? "" : JSON.stringify(data);
+        d.addEventListener("action", function() {
+            callback && callback(JSON.parse(d.value));
+            d.parentNode.removeChild(d);
+        }, true)
+        document.body.appendChild(d);
+
+        e.initEvent(EVENT_NAME, false, true);
+        d.dispatchEvent(e);
+    }
+
+    function xhttp(data, callback) {
+        sendMessage({type: REQUEST_TYPE_XHTTP, body: data}, callback);
     }
 
     return {init: init};
